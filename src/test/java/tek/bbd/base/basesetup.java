efc7c3b7567e141b292b83d6e775acbb113585da
musawerpopal <mnsrpopal1@gamil.com>
2024-08-01 21:43:07 -0700

    setup class completed

---
+++ src/test/java/tek/bbd/base/basesetup.java
@@ -0,0 +1,26 @@
+package tek.bbd.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class basesetup {
+
+     private static WebDriver driver;
+     public void setupbrowser() {
+
+        driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
+    public void quitbrowser() {
+        if(driver != null) {
+            driver.quit();
+        }
+    }
+    public WebDriver getDriver() {
+         return driver;
+    }
+}
